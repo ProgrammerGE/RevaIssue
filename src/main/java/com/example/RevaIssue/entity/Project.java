d5126ce1071f9dfc15ed69ca3b5bde6bf638c983
ChrisJacobsJr <bluflare12345@gmail.com>
2025-12-18 15:36:22 -0500

    progress on projects entity

--- src/main/java/com/example/RevaIssue/entity/Project.java
+++ src/main/java/com/example/RevaIssue/entity/Project.java
@@ -1,4 +1,23 @@
 package com.example.RevaIssue.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Projects")
 public class Project {
+    
+
+    @Id
+    @Column(name = "project_ID", nullable = false)
+    private int projectID;
+    @Column(name = "Project_Name", nullable = false)
+    private String projectName;
+    @Column(name = "Project_Description", nullable = true)
+    private String projectDescription;
 }
