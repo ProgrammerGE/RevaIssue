446b7c431803fbab8aaee2fd56803668405698cb
ProgrammerGE <gabrielevans96@gmail.com>
2025-12-18 12:50:12 -0800

    Filled in the barebones of Users and User_Project Entity

--- src/main/java/com/example/RevaIssue/entity/User.java
+++ src/main/java/com/example/RevaIssue/entity/User.java
@@ -1,4 +1,25 @@
 package com.example.RevaIssue.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
 public class User {
+
+    @Id
+    @Column(name = "User_ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID user_ID;
+    @Column
+    private String username;
+    @Column
+    private String password_hash;
+    @Column(name = "User_Role", nullable = false)
+    private String user_Role;
 }
