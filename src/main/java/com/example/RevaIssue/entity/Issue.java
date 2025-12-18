bb4fb583aa08421fad9d800d486d166858d47b55
ProgrammerGE <gabrielevans96@gmail.com>
2025-12-18 13:19:07 -0800

    Fixed an error with the "Issues" entity and repo by adding ';' semi-colons, and edited the style by tabbing some lines over.

--- src/main/java/com/example/RevaIssue/entity/Issue.java
+++ src/main/java/com/example/RevaIssue/entity/Issue.java
@@ -1,4 +1,8 @@
-package com.example.RevaIssue.entity
+package com.example.RevaIssue.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -6,20 +10,20 @@ package com.example.RevaIssue.entity
 @Table(name = "issues")
 public class Issue {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "issue_id", nullable = false)
-private Long issueID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "issue_id", nullable = false)
+    private Long issueID;
 
-@Column(name = "issue_description")
-private String description;
+    @Column(name = "issue_description")
+    private String description;
 
-@Column(name = "project_id", nullable = false)
-private Integer projectId;
+    @Column(name = "project_id", nullable = false)
+    private Integer projectId;
 
-@Column(name = "severity")
-private Integer severity;
+    @Column(name = "severity")
+    private Integer severity;
 
-@Column(name = "priority")
-private Integer priority;
+    @Column(name = "priority")
+    private Integer priority;
 }
