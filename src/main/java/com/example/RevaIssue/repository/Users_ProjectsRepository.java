4c924f8fe5d3e09ba9a2bb0c1d3d81e3d3a71043
ChrisJacobsJr <bluflare12345@gmail.com>
2025-12-19 14:39:28 -0500

    Added @Repository annotation

--- src/main/java/com/example/RevaIssue/repository/Users_ProjectsRepository.java
+++ src/main/java/com/example/RevaIssue/repository/Users_ProjectsRepository.java
@@ -2,6 +2,8 @@ package com.example.RevaIssue.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.RevaIssue.entity.Users_Projects;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface Users_ProjectsRepository extends JpaRepository<Users_Projects, Integer> {
 }
