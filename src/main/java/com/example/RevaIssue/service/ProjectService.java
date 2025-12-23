703d1ea34471be25d5a6f8de2823a2bd458f4b05
ProgrammerGE <gabrielevans96@gmail.com>
2025-12-23 13:31:33 -0800

    Added Issue/Project Service

---
+++ src/main/java/com/example/RevaIssue/service/ProjectService.java
@@ -0,0 +1,44 @@
+package com.example.RevaIssue.service;
+
+import com.example.RevaIssue.entity.Project;
+import com.example.RevaIssue.entity.User;
+import com.example.RevaIssue.entity.User_Projects;
+import com.example.RevaIssue.repository.ProjectRepository;
+import com.example.RevaIssue.repository.User_ProjectsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepo;
+
+    @Autowired
+    private User_ProjectsRepository user_projectsRepo;
+
+    public Project createProject(Project project){
+        return projectRepo.save(project);
+    }
+
+    public boolean deleteProject(int projId){
+        Project targetProject = projectRepo.getReferenceById(projId);
+        if(targetProject == null){
+            return false;
+        }
+        projectRepo.deleteById(projId);
+        return true;
+    }
+
+    public List<User> getAllUsersByProject(Project project){
+        List<User_Projects> userProjectList = user_projectsRepo.findByProject(project);
+        List<User> users = new ArrayList<>();
+        for(User_Projects userProj : userProjectList){
+            users.add(userProj.getUser());
+        }
+        return users;
+    }
+}
