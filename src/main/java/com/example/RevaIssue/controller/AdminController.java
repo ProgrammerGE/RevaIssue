70a0ab4bc44e268c1f29aec816aa01dc627376db
ChrisJacobsJr <bluflare12345@gmail.com>
2025-12-23 15:19:44 -0500

    Added admin controller skeleton

--- src/main/java/com/example/RevaIssue/controller/AdminController.java
+++ src/main/java/com/example/RevaIssue/controller/AdminController.java
@@ -1,4 +1,31 @@
 package com.example.RevaIssue.controller;
 
+import com.example.RevaIssue.entity.Project;
+import com.example.RevaIssue.repository.IssueRepository;
+import com.example.RevaIssue.repository.ProjectRepository;
+import com.example.RevaIssue.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+
+    // One of these may not be necessary TODO : Remove if not necessary after MVP
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private IssueRepository issueRepository;
+
+//    Dummy endpoint for example purposes
+//    @PostMapping("/admin")
+//    public Project addProject(@RequestBody Project project) {
+//        return projectRepository.save(project);
+//    }
+
+
+
 }
