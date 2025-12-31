c4b44c9db3954d7fdf52b9ec7f93fa7adcc41bfe
ChrisJacobsJr <bluflare12345@gmail.com>
2025-12-31 16:00:17 -0500

    initialized the create-issue popup with a grid setup and comments

--- frontend1/src/app/app.routes.ts
+++ frontend1/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Projects } from './components/projects/projects';
 import { Issues } from './components/issues/issues';
 import { ProjectDetails } from './components/project-details/project-details';
 import { IssueDetails } from './components/issue-details/issue-details';
+import { CreateIssue } from './components/create-issue/create-issue';
 
 export const routes: Routes = [
     {path: 'login', component:Login},
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path: 'issues', component:Issues},
     // test route, TODO: correct later
     {path: 'project-details', component:ProjectDetails},
-    {path: 'issue-details', component:IssueDetails}
+    {path: 'issue-details', component:IssueDetails},
+    {path: 'create-issue', component:CreateIssue}
 ];
---
+++ frontend1/src/app/components/create-issue/create-issue.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-issue',
+  imports: [],
+  templateUrl: './create-issue.html',
+  styleUrl: './create-issue.css',
+})
+export class CreateIssue {
+
+}
