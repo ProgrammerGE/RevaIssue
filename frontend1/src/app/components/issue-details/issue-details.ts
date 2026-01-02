413e9953adb31ee0fb220dc448e79d30ce450ec0
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-02 10:53:54 -0500

    Fleshed out the issue-details page with buttons for claiming/resolving as well as an edit mode for the testers

--- frontend1/src/app/components/issue-details/issue-details.ts
+++ frontend1/src/app/components/issue-details/issue-details.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-issue-details',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './issue-details.html',
   styleUrl: './issue-details.css',
 })
 export class IssueDetails {
   issueTitle: string = 'Test';
+  issueDescription: string = '';
+
+  userRole: 'tester' | 'developer' | 'admin' = 'tester';
+  isEditing: boolean = false;
+
+  // Button Actions
+  onEdit() {
+    this.isEditing = true;
+  }
+  onSubmit() {
+    this.isEditing = false;
+    alert('Issue updated!');
+  }
+  onClaim() {
+    alert('Issue claimed by you!');
+  }
+  onResolve() {
+    alert('Issue marked as resolved!');
+  }
 }
