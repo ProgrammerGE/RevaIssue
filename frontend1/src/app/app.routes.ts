992b5044d50ea28c43fda36f4643b80cc76d9c72
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-02 11:45:17 -0500

    removed route for refactor of issue-details

--- frontend1/src/app/app.routes.ts
+++ frontend1/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { HubPage } from './components/hub-page/hub-page';
 import { Project } from './components/project/project';
 import { Issue } from './components/issue/issue';
 import { ProjectDetails } from './components/project-details/project-details';
-import { IssueDetails } from './components/issue-details/issue-details';
 import { CreateIssue } from './components/create-issue/create-issue';
 
 export const routes: Routes = [
@@ -17,6 +16,5 @@ export const routes: Routes = [
     {path: 'issue', component:Issue},
     // test route, TODO: correct later
     {path: 'project-details', component:ProjectDetails},
-    {path: 'issue-details', component:IssueDetails},
     {path: 'create-issue', component:CreateIssue}
 ];
