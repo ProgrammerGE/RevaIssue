b39a9375b24e26350096ecb618de2db4df528f4f
Juan Sierra <sierra.juan99@gmail.com>
2026-01-04 00:00:25 -0800

    feat: new hubpage design
    - new <app-list-container /> component for displaying projects and issues lists
    - css and design overhaul

--- frontend1/src/app/components/hub-page/hub-page.ts
+++ frontend1/src/app/components/hub-page/hub-page.ts
@@ -1,19 +1,36 @@
 import { Component, signal, WritableSignal } from '@angular/core';
+import { ListContainer } from '../list-container/list-container';
 import { Projects } from '../projects/projects';
-import { Issues } from '../issues/issues';
 import { SignoutButton } from '../signout-button/signout-button';
 import { LoginService } from '../../services/login-service';
 import { Router, RouterLink } from '@angular/router';
+import { hubListItem } from '../../interfaces/hubpage-list-item';
 
 @Component({
   selector: 'app-hub-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ListContainer],
   templateUrl: './hub-page.html',
   styleUrl: './hub-page.css',
 })
 export class HubPage {
+  username: string = 'username';
+  userRole: string = 'role';
+  issuesCount: string = '0';
+
+  projectsList: hubListItem[] = [
+    { name: 'Project 1', description: 'this is a description for project 1' },
+    { name: 'Project 2', description: 'this is a desription for project 2' },
+    { name: 'Project 3', description: 'this is a description for projet 3' },
+  ];
+
+  issuesList: hubListItem[] = [
+    { name: 'Issue 1', description: 'this is a description for project 1' },
+    { name: 'Issue 2', description: 'this is a desription for issue 2' },
+    { name: 'Issue 3', description: 'this is a description for issue 3' },
+    { name: 'Issue 4', description: 'this is a description for issue 4' },
+  ];
 
   userLoggedIn: WritableSignal<boolean> = signal(false);
 
-  constructor(private loginService: LoginService, private router: Router){}
+  constructor(private loginService: LoginService, private router: Router) {}
 }
---
+++ frontend1/src/app/components/list-container/list-container.ts
@@ -0,0 +1,21 @@
+import { Component, input, InputSignal, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { hubListItem } from '../../interfaces/hubpage-list-item';
+
+@Component({
+  selector: 'app-list-container',
+  imports: [CommonModule],
+  templateUrl: './list-container.html',
+  styleUrl: './list-container.css',
+})
+export class ListContainer {
+  title = input<string>('Title');
+  isExpanded = signal(true);
+  items: InputSignal<hubListItem[]> = input([{ name: 'placeholder title', description: 'placeholder description' }]);
+  itemCount = computed(() => this.items().length);
+
+  expandList() {
+    this.isExpanded.update((v) => !v);
+  }
+}
+
