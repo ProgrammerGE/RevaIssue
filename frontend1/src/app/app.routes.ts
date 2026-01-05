828b6144775ba9a35437f4b916bc08a74c013e7a
ProgrammerGE <gabrielevans96@gmail.com>
2026-01-05 13:37:42 -0800

    Created a pop-up window on the hub page (needs some CSS tuning)

--- frontend1/src/app/app.routes.ts
+++ frontend1/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Project } from './components/project/project';
 import { Issue } from './components/issue/issue';
 import { CreateIssue } from './components/create-issue/create-issue';
 import { authorizationGuard } from './guards/authorization-guard';
+import { CreateProject } from './components/create-project/create-project';
 
 export const routes: Routes = [
     {path:'', pathMatch:'full', redirectTo:'/login'},
@@ -14,5 +15,6 @@ export const routes: Routes = [
     {path: 'hubpage', component:HubPage, canActivate:[authorizationGuard]},
     {path: 'project', component:Project, canActivate:[authorizationGuard]},
     {path: 'issue', component:Issue, canActivate:[authorizationGuard]},
-    {path: 'create-issue', component:CreateIssue, canActivate:[authorizationGuard]}
+    {path: 'create-issue', component:CreateIssue, canActivate:[authorizationGuard]},
+    {path: 'create-project', component:CreateProject, canActivate:[authorizationGuard]}
 ];
--- frontend1/src/app/components/list-container/list-container.ts
+++ frontend1/src/app/components/list-container/list-container.ts
@@ -1,6 +1,7 @@
 import { Component, input, InputSignal, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { hubListItem } from '../../interfaces/hubpage-list-item';
+import { PopUpService } from '../../services/pop-up-service';
 
 @Component({
   selector: 'app-list-container',
@@ -14,8 +15,14 @@ export class ListContainer {
   items: InputSignal<hubListItem[]> = input([{ name: 'placeholder title', description: 'placeholder description' }]);
   itemCount = computed(() => this.items().length);
 
+  constructor(private popUpService: PopUpService){}
+
   expandList() {
     this.isExpanded.update((v) => !v);
+  }
+
+  addProjectPopup(){
+    this.popUpService.openPopUp();
   }
 }
 
