8dfce538498bde737dd96a1b6e1b8a4195108013
Eliwash73 <washingtonelijah73@gmail.com>
2026-01-06 12:45:20 -0500

    merge from main error fixes

--- frontend1/src/app/components/create-project/create-project.ts
+++ frontend1/src/app/components/create-project/create-project.ts
@@ -12,46 +12,48 @@ import { PopUpService } from '../../services/pop-up-service';
   styleUrl: './create-project.css',
 })
 export class CreateProject {
-
-  projectTitle: string = "";
-  projectDesc: string = "";
-  buttonText = "Create Project";
-  buttonCancel = "Cancel";
+  projectTitle: string = '';
+  projectDesc: string = '';
+  buttonText = 'Create Project';
+  buttonCancel = 'Cancel';
   isPoppedUp: WritableSignal<boolean> = signal(false);
 
   titleMissing: boolean = false;
   descriptionMissing: boolean = false;
 
-
-  constructor(private popUpService: PopUpService, private projectService: ProjectService, private router: Router) {
-    this.popUpService.getPopUpProjectSubject().subscribe( popUpSetting => {
+  constructor(
+    private popUpService: PopUpService,
+    private projectService: ProjectService,
+    private router: Router
+  ) {
+    this.popUpService.getPopUpProjectSubject().subscribe((popUpSetting) => {
       this.isPoppedUp.set(popUpSetting);
-    })
+    });
   }
 
-  createProject(){ //Following the same format as on the project.ts file
+  createProject() {
+    //Following the same format as on the project.ts file
     this.titleMissing = false;
     this.descriptionMissing = false;
-    
-    if(this.projectTitle != "" &&  this.projectDesc != ""){
+
+    if (this.projectTitle != '' && this.projectDesc != '') {
       this.projectService.createProject({
-        project_name: this.projectTitle,
-        project_description: this.projectDesc
-      });    
+        projectName: this.projectTitle,
+        projectDescription: this.projectDesc,
+      });
       this.isPoppedUp.set(false);
     }
 
-    if(this.projectTitle == ""){
+    if (this.projectTitle == '') {
       this.titleMissing = true;
     }
 
-    if(this.projectDesc == ""){
+    if (this.projectDesc == '') {
       this.descriptionMissing = true;
     }
-
   }
 
-  cancelCreation(){
+  cancelCreation() {
     this.isPoppedUp.set(false);
   }
 }
