6fafb6ec3c0fb7ccb14f3263f2771073d1517de9
Juan Sierra <sierra.juan99@gmail.com>
2026-01-06 11:24:39 -0800

    get most recent issues

--- backend/src/main/java/com/example/RevaIssue/controller/CommonController.java
+++ backend/src/main/java/com/example/RevaIssue/controller/CommonController.java
@@ -73,6 +73,6 @@ public class CommonController {
 
     @GetMapping("/issues/latest")
     public List<Issue> getMostRecentIssues() {
-        return issueService.getMostRecent();
+        return issueService.getMostRecentIssues();
     }
 }
