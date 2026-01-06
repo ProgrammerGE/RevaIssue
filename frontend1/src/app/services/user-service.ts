5682f40092dfa0a2283dd0f68c944bcae0bca43e
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-06 15:12:34 -0500

    updated todo comments

---
+++ frontend1/src/app/services/user-service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+
+  // get all users method (to be used by admin only)
+
+  // assign user to project method (to be used by admin only)
+
+  // get all users in a project method (to be used by users of that project only)
+
+
+}
