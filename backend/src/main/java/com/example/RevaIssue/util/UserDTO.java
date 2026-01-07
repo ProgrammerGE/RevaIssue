c486101008dbe355409eb6ac0fe5e88b96bfeb42
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-06 21:25:40 -0500

    renamed UserRole userRole to UserRole role for compatibility with the frontend

--- backend/src/main/java/com/example/RevaIssue/util/UserDTO.java
+++ backend/src/main/java/com/example/RevaIssue/util/UserDTO.java
@@ -1,10 +1,7 @@
 package com.example.RevaIssue.util;
-import com.example.RevaIssue.entity.User;
 import com.example.RevaIssue.enums.UserRole;
-
-import java.util.UUID;
 
 public record UserDTO(
         String username,
-        UserRole userRole
+        UserRole role
 ) {}
