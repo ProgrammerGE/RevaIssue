9d8b0d7de0e436519f7f10abfcdc07ac31ce5546
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-07 01:43:00 -0500

    added custom method to get users who are associated with the project

--- backend/src/main/java/com/example/RevaIssue/repository/User_ProjectsRepository.java
+++ backend/src/main/java/com/example/RevaIssue/repository/User_ProjectsRepository.java
@@ -4,6 +4,8 @@ import com.example.RevaIssue.entity.Project;
 import com.example.RevaIssue.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.RevaIssue.entity.User_Projects;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ import java.util.List;
 public interface User_ProjectsRepository extends JpaRepository<User_Projects, Integer> {
     List<User_Projects> findByUser(User user);
     List<User_Projects> findByProject(Project project);
+
+    @Query("SELECT up.user FROM User_Projects up WHERE up.project.projectID = :pId")
+    List<User> findUsersByProjectId(@Param("pId") int pId);
 }
