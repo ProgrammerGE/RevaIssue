d980164cea587e4fc76b3ddd10116beed854cec8
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-08 15:26:29 -0500

    changed the viewAllProjects method in the service to use the common endpoint, instead of selecting one based on the user

--- frontend1/src/app/services/project-service.ts
+++ frontend1/src/app/services/project-service.ts
@@ -48,7 +48,7 @@ export class ProjectService {
   // TODO: user role should only be of type admin | developer | tester
   viewAllProjects(projects: WritableSignal<Array<ProjectData>>, role: string): void {
     this.httpClient
-      .get<ProjectData[]>(`${this.baseUrl}/${role}/projects`)
+      .get<ProjectData[]>(`${this.baseUrl}/common/projects`)
       .subscribe((projectList) => {
         const newProjectList = [];
         for (const projectObj of projectList) {
