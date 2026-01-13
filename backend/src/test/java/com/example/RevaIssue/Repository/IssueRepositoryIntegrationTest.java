25b5c5248b191fcbab7bf050c4d5764c6ebee8bd
ChrisJacobsJr <bluflare12345@gmail.com>
2026-01-13 11:51:06 -0500

    finished second test and utilized the @beforeeach flag

--- backend/src/test/java/com/example/RevaIssue/Repository/IssueRepositoryIntegrationTest.java
+++ backend/src/test/java/com/example/RevaIssue/Repository/IssueRepositoryIntegrationTest.java
@@ -4,6 +4,7 @@ import com.example.RevaIssue.entity.Issue;
 import com.example.RevaIssue.entity.Project;
 import com.example.RevaIssue.repository.IssueRepository;
 import com.example.RevaIssue.repository.ProjectRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -21,20 +22,25 @@ public class IssueRepositoryIntegrationTest {
     private final IssueRepository issueRepository;
     private final ProjectRepository projectRepository;
 
+    private Project project;
+
     @Autowired
     public  IssueRepositoryIntegrationTest(IssueRepository issueRepository,  ProjectRepository projectRepository) {
         this.issueRepository = issueRepository;
         this.projectRepository = projectRepository;
     }
 
-    @Test
-    void findByProjectIdPositiveTest() {
-        // initialize the project the issue needs to exist
-        Project project = new Project();
+    @BeforeEach
+    void setUp() {
+        project = new Project();
         project.setProjectName("projectName");
         project.setProjectDescription("projectDescription");
-        long projectId = projectRepository.save(project).getProjectID();
+        project = projectRepository.save(project);
 
+    }
+
+    @Test
+    void findByProjectIdPositiveTest() {
         // initialize the issue to be used
         Issue issue = new Issue();
         issue.setName("Test issue");
@@ -45,7 +51,7 @@ public class IssueRepositoryIntegrationTest {
         issue.setStatus("open"); // may be redundant
         issueRepository.save(issue);
 
-        List<Issue> issueList = issueRepository.findByProjectProjectID(projectId);
+        List<Issue> issueList = issueRepository.findByProjectProjectID((long)project.getProjectID());
 
         // ensure the list has stuff innit
         assertTrue(issueList.contains(issue));
@@ -58,13 +64,13 @@ public class IssueRepositoryIntegrationTest {
         // Step 1: look for issues in a project that doesn't exist:
         issues = issueRepository.findByProjectProjectID((long)9999);
         assertTrue(issues.isEmpty());
-        // Step 2: look for issues in a project that shouldn't have any:
-        Project project = new Project();
-        project.setProjectName("projectName");
-        project.setProjectDescription("projectDescription");
-        project = projectRepository.save(project);
-
+        // Step 2: look for issues in a project without them
         issues = issueRepository.findByProjectProjectID((long)project.getProjectID());
         assertTrue(issues.isEmpty());
+    }
+
+    @Test
+    void findByKeyWordPositiveTest() {
+
     }
 }
