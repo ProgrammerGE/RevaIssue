d7d4f4cdac98de98ceadbe0c3d65305d759354c5
Juan Sierra <sierra.juan99@gmail.com>
2026-01-15 13:18:11 -0800

    Update login page to use forms

--- frontend1/src/app/components/login/login.ts
+++ frontend1/src/app/components/login/login.ts
@@ -1,38 +1,36 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { JwtTokenStorage } from '../../services/jwt-token-storage';
 import { TokenData } from '../../interfaces/token-data';
 import { UserService } from '../../services/user-service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  username = '';
-  password = '';
-
   httpClient = inject(HttpClient);
   router = inject(Router);
   jwtStorage = inject(JwtTokenStorage);
+  userService = inject(UserService)
+  private formBuilder = inject(FormBuilder);
+  
 
-  constructor(private userService: UserService) {}
+  loginForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
   onSubmit() {
     this.httpClient
-      .post<TokenData>(
-        'http://localhost:8080/auth/login',
-        {
-          username: this.username,
-          password: this.password,
-        },
-        { observe: 'response' }
-      )
+      .post<TokenData>('http://localhost:8080/auth/login', this.loginForm.value, {
+        observe: 'response',
+      })
       .subscribe({
         next: (response) => {
           if (response.body) {
