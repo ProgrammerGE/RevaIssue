46f2262ab2e1bb4c3a6c0176029671a46e9440c4
Juan Sierra <sierra.juan99@gmail.com>
2026-01-15 11:21:38 -0800

    update login page to  use form() signal

--- frontend1/src/app/components/login/login.ts
+++ frontend1/src/app/components/login/login.ts
@@ -1,36 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { JwtTokenStorage } from '../../services/jwt-token-storage';
 import { TokenData } from '../../interfaces/token-data';
 import { UserService } from '../../services/user-service';
+import { LoginForm } from '../../interfaces/login-form';
+import { form, Field } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, Field],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  username = '';
-  password = '';
+  loginModel = signal<LoginForm>({
+    username: '',
+    password: '',
+  });
 
+  loginForm = form(this.loginModel, (scemaPath) => {
+    
+  });
   httpClient = inject(HttpClient);
   router = inject(Router);
   jwtStorage = inject(JwtTokenStorage);
 
-  constructor( private userService: UserService,){
-
-  }
+  constructor(private userService: UserService) {}
 
   onSubmit() {
+    console.log(this.loginForm.username().value())
     this.httpClient
       .post<TokenData>(
         'http://localhost:8080/auth/login',
         {
-          username: this.username,
-          password: this.password,
+          username: this.loginForm.username().value(),
+          password: this.loginForm.password().value(),
         },
         { observe: 'response' }
       )
