77f319ea80ae3090012a5d41733d92b168065c90
Juan Sierra <sierra.juan99@gmail.com>
2026-01-16 11:01:14 -0800

    Create Driver.java
    - initializes driver on startup
    - removes driver on end

---
+++ backend/src/test/java/com/example/RevaIssue/E2E/hooks/Driver.java
@@ -0,0 +1,24 @@
+package com.example.RevaIssue.E2E.hooks;
+
+import com.example.RevaIssue.E2E.driver.ChromeDriverManager;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
+import io.cucumber.spring.ScenarioScope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+public class Driver {
+    @Autowired
+    ChromeDriverManager driverManager;
+
+    @Before
+    public void setupDriver() {
+        driverManager.getDriver();
+    }
+
+    @After
+    public void teardownDriver() {
+        driverManager.quit();
+    }
+}
