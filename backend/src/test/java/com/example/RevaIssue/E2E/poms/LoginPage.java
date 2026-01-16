90956b21857f6c4c898b03ce249d5cdce050a01e
Juan Sierra <sierra.juan99@gmail.com>
2026-01-16 14:00:05 -0800

    Refactor: Poms now use ChromeDriverManager  class

--- backend/src/test/java/com/example/RevaIssue/E2E/poms/LoginPage.java
+++ backend/src/test/java/com/example/RevaIssue/E2E/poms/LoginPage.java
@@ -2,7 +2,7 @@ package com.example.RevaIssue.E2E.poms;
 
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage extends ParentPOM{
+public class LoginPage {
     private final String URL = "http://localhost:4200/hubpage";
     public LoginPage(WebDriver driver) {
         super(driver);
