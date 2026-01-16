2b58da6b0b42ef86ff9cf592960720673ec3550d
Juan Sierra <sierra.juan99@gmail.com>
2026-01-16 14:01:54 -0800

    Update LoginPage.java

--- backend/src/test/java/com/example/RevaIssue/E2E/poms/LoginPage.java
+++ backend/src/test/java/com/example/RevaIssue/E2E/poms/LoginPage.java
@@ -1,10 +1,17 @@
 package com.example.RevaIssue.E2E.poms;
 
+import com.example.RevaIssue.E2E.driver.ChromeDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class LoginPage {
     private final String URL = "http://localhost:4200/hubpage";
-    public LoginPage(WebDriver driver) {
-        super(driver);
+    private final WebDriver driver;
+
+    @Autowired
+    public LoginPage(ChromeDriverManager chromeDriverManager){
+        this.driver = chromeDriverManager.getDriver();
+        PageFactory.initElements(driver, this);
     }
 }
