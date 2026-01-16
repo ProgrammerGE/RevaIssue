bf441dd2ad6fc24d588e68cd81de6a37d4c5c417
Juan Sierra <sierra.juan99@gmail.com>
2026-01-16 13:51:27 -0800

    add: Auth helper authentication

---
+++ backend/src/test/java/com/example/RevaIssue/E2E/helper/AuthHelper.java
@@ -0,0 +1,43 @@
+package com.example.RevaIssue.E2E.helper;
+
+import com.example.RevaIssue.E2E.driver.ChromeDriverManager;
+import com.example.RevaIssue.dto.LoginRequest;
+import com.example.RevaIssue.entity.User;
+import com.example.RevaIssue.enums.UserRole;
+import com.example.RevaIssue.service.AuthService;
+import com.example.RevaIssue.service.UserService;
+import io.cucumber.spring.ScenarioScope;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@ScenarioScope
+public class AuthHelper {
+    private final WebDriver driver;
+    private final UserService userService;
+    private final AuthService authService;
+
+
+    @Autowired
+    public AuthHelper(ChromeDriverManager driverManager, UserService userService, AuthService authService) {
+        this.driver = driverManager.getDriver();
+        this.userService = userService;
+        this.authService = authService;
+    }
+
+    public void authenticateUser(UserRole role) {
+        User user = new User();
+        user.setUsername("user@mail.com");
+        user.setPassword("#@!user-strong-password!@#");
+        user.setUserRole(role);
+        userService.createUser(user);
+        LoginRequest request = new LoginRequest(user.getUsername(), user.getPassword());
+        String token = authService.login(request);
+        ((JavascriptExecutor) driver).executeScript(
+                "window.localStorage.setItem('REVAISSUE_TOKEN', arguments[0]);",
+                token
+        );
+    }
+}
