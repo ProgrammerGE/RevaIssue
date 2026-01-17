09b952f600a034c13236b0b0785e02adc4708d72
Juan Sierra <sierra.juan99@gmail.com>
2026-01-16 17:11:04 -0800

    feat: finish feature steps

--- backend/src/test/java/com/example/RevaIssue/E2E/steps/search/SearchIssues.java
+++ backend/src/test/java/com/example/RevaIssue/E2E/steps/search/SearchIssues.java
@@ -3,51 +3,51 @@ package com.example.RevaIssue.E2E.steps.search;
 import com.example.RevaIssue.E2E.helper.AuthHelper;
 import com.example.RevaIssue.E2E.poms.HubPage;
 import com.example.RevaIssue.E2E.poms.LoginPage;
+import com.example.RevaIssue.E2E.poms.ProjectPage;
 import com.example.RevaIssue.enums.UserRole;
-import com.example.RevaIssue.repository.UserRepository;
-import com.example.RevaIssue.service.AuthService;
-import com.example.RevaIssue.service.ProjectService;
-import com.example.RevaIssue.service.UserService;
-import com.example.RevaIssue.util.JwtUtility;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.ScenarioScope;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @ScenarioScope
 public class SearchIssues {
     private final LoginPage loginPage;
+    private final ProjectPage projectPage;
     private final HubPage hubPage;
     private final AuthHelper authHelper;
 
-    public SearchIssues (LoginPage loginPage, HubPage hubPage, AuthHelper authHelper) {
+    public SearchIssues (LoginPage loginPage, HubPage hubPage, AuthHelper authHelper, ProjectPage projectPage) {
         this.loginPage = loginPage;
         this.hubPage = hubPage;
         this.authHelper = authHelper;
+        this.projectPage = projectPage;
     }
 
-    @Given("The user is logged in")
-    public void theUserIsLoggedIn() throws InterruptedException {
+    @Given("The user is logged into the hubpage")
+    public void theUserIsLoggedIn() {
         this.loginPage.goToLogin();
         authHelper.authenticateUser(UserRole.TESTER);
-        this.loginPage.goToHubpage();
-        this.loginPage.createProject();
-        this.loginPage.createIssue();
-//        Thread.sleep(20000);
+        this.hubPage.openHubPage();
     }
 
-    @Then("The user can search for an issue")
-    public void theUserCanSearchForAnIssue() throws InterruptedException {
-        this.hubPage.enableSearchPopup();
-        Thread.sleep(200000);
+    @When("The user searches for an issue")
+    public void theUserCanSearchForAnIssue() {
+        this.hubPage.toggleSearchPopup();
+        this.hubPage.searchForIssue();
     }
-//
-//    @Then("The user can search for an issue")
-//    public void theUserCanSearch() throws InterruptedException {
-//        WebElement searchBar = driver.findElement(By.id("searchbar"));
-//        searchBar.click();
-//        WebElement searchPopup = driver.findElement(By.cssSelector("div.search-container > input"));
-//        searchPopup.sendKeys("Test");
-//        Thread.sleep(100000);
-//    }
+
+    @And("The user clicks on a result")
+    public void theUserClicksOnAResult() {
+        this.hubPage.selectIssueSearchResult();
+    }
+
+    @Then("The user is sent to the issue's project page")
+    public void theUserIsSentToIssuesProjectPage() {
+        boolean onProjectPage = this.projectPage.isOnProjectPage();
+        if (!onProjectPage) {
+            throw new AssertionError("redirect to project page failed");
+        }
+    }
 }
