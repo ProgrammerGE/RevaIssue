6675f5e6e7e197d38b7b707549a80080399ee275
Juan Sierra <sierra.juan99@gmail.com>
2026-01-17 21:24:47 -0800

    refactor: Dependency Injection now being used for hubpage pom

--- backend/src/test/java/com/example/RevaIssue/E2E/steps/popups/submitting/SubmitDeleteProject.java
+++ backend/src/test/java/com/example/RevaIssue/E2E/steps/popups/submitting/SubmitDeleteProject.java
@@ -1,23 +1,28 @@
 package com.example.RevaIssue.E2E.steps.popups.submitting;
 
-import io.cucumber.java.en.Given;
+import com.example.RevaIssue.E2E.poms.HubPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 
-import static com.example.RevaIssue.E2E.fixtures.FixtureResources.hubpage;
-
 public class SubmitDeleteProject {
+    private final HubPage hubpage;
+
+    public SubmitDeleteProject(HubPage hubpage) {
+        this.hubpage = hubpage;
+    }
+
     @When("The admin clicks on the delete project button")
     public void the_admin_clicks_on_the_create_project_button(){
-        hubpage.clickDeleteProject();
+        this.hubpage.clickDeleteProject();
     }
     @When("clicks the confirm delete button")
     public void clicks_the_confirm_delete_button(){
-        hubpage.clickDeleteConfirm();
+        this.hubpage.clickDeleteConfirm();
     }
+
     @Then("A project will be delete")
     public void a_project_will_be_delete() {
-        Assertions.assertTrue(hubpage.isDeletePopupOpen());
+        Assertions.assertTrue(this.hubpage.isDeletePopupOpen());
     }
 }
